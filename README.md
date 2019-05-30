4bba11fa2e5ad280d6bb7d12519aedc9c9e0b9c5
paramagicdev <konnor5456@gmail.com>
2019-05-30 01:16:53 -0400

    finished part2, addin cheatsheets

--- README.md
+++ README.md
@@ -2,4 +2,26 @@
 
 ## This is directly taken from [Docker's docs](https://docs.docker.com/get-started)
 
+### Part 1 cheatsheet
 
+```dockerfile
+## List Docker CLI commands
+docker
+docker container --help
+
+## Display Docker version and info
+docker --version
+docker version
+docker info
+
+## Execute Docker image
+docker run hello-world
+
+## List Docker images
+docker image ls
+
+## List Docker containers (running, all, all in quiet mode)
+docker container ls
+docker container ls --all
+docker container ls -aq
+```
--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
-from redist import Redis, RedisError
+from redis import Redis, RedisError
 import os
 import socket
 
-# connect to Redis
+# Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
     try:
         visits = redis.incr("counter")
@@ -18,9 +18,7 @@ def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
-
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
-# run on port 80 if its the main function
-if __name__ == "__main__"
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
